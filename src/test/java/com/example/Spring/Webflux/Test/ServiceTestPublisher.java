201e34ae5dc48bb38414dd6ca2fc9b1c2f18e2f3
juanesosorio <Juanchis_1989@hotmail.com>
2021-12-10 23:22:52 -0500

    se implementa el ServiceTestPublisher

--- src/test/java/com/example/Spring/Webflux/Test/ServiceTestPublisher.java
+++ src/test/java/com/example/Spring/Webflux/Test/ServiceTestPublisher.java
@@ -1,6 +1,7 @@
 package com.example.Spring.Webflux.Test;
 
 import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 import reactor.test.publisher.TestPublisher;
 
 public class ServiceTestPublisher {
@@ -12,6 +13,17 @@ public class ServiceTestPublisher {
                 .next("Primero", "Segundo", "Tercero")
                 .error(new RuntimeException("Message"));
 
+    }
+
+
+    @Test
+    void testUpperCase() {
+        final TestPublisher<String> testPublisher = TestPublisher.create();
+        UppercaseConverter uppercaseConverter = new UppercaseConverter(testPublisher.flux());
+        StepVerifier.create(uppercaseConverter.getUpperCase())
+                .then(() -> testPublisher.emit("datos", "GeNeRaDoS", "Sofka"))
+                .expectNext("DATOS", "GENERADOS", "SOFKA")
+                .verifyComplete();
     }
 
 }
